5cf554b125750190ca178f0be269e7390e03b37c
Kris <kpelt@softserveinc.com>
2022-12-05 22:57:53 +0200

    rename var

--- dist/utils/countBy.js
+++ dist/utils/countBy.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.countBy = void 0;
 const countBy = (iterable, fn) => {
-    const aggregate = new Map();
+    const map = new Map();
     for (const elem of iterable) {
         const mapKey = fn(elem);
-        const count = aggregate.get(mapKey) || 0;
-        aggregate.set(mapKey, count + 1);
+        const count = map.get(mapKey) || 0;
+        map.set(mapKey, count + 1);
     }
-    return aggregate;
+    return map;
 };
 exports.countBy = countBy;
