eb5f1bbfba68e6ad1711a40c81ad97905eca2f05
Kris <kpelt@softserveinc.com>
2022-12-05 11:22:52 +0200

    day 5

--- dist/5/b.js
+++ dist/5/b.js
@@ -6,6 +6,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("../utils");
 const input_1 = __importDefault(require("./input"));
 (function () {
-    const result = (0, utils_1.flow)(input_1.default).pipe(utils_1.splitByLine);
+    const lines = (0, utils_1.splitByLine)(input_1.default);
+    const stackNumbersLineIndex = lines.findIndex((line) => line === "") - 1;
+    const lineOfStackNumber = lines[stackNumbersLineIndex];
+    const stackLines = lines.slice(0, stackNumbersLineIndex);
+    const instructionLines = lines.slice(stackNumbersLineIndex + 2);
+    const charPositions = [];
+    lineOfStackNumber.split("").forEach((char, i) => {
+        if (char !== " ") {
+            charPositions.push(i);
+        }
+    });
+    const stacks = new Map();
+    stackLines.forEach((line) => {
+        charPositions.forEach((charPos, stackIdx) => {
+            const stackKey = stackIdx + 1;
+            const stack = stacks.get(stackKey) || [];
+            const val = line[charPos];
+            if (val !== " ") {
+                stack.push(val);
+                stacks.set(stackKey, stack);
+            }
+        });
+    });
+    instructionLines.forEach((line) => {
+        const [move, from, to] = line.split(" ").filter(Number).map(Number);
+        const fromStack = stacks.get(from);
+        const toStack = stacks.get(to);
+        if (fromStack && toStack) {
+            const moved = fromStack.splice(0, move);
+            toStack.unshift(...moved);
+        }
+    });
+    const result = Array.from(stacks.entries())
+        .sort((a, b) => a[0] - b[0])
+        .reduce((str, [_, stack]) => {
+        str += stack[0];
+        return str;
+    }, "");
     console.log(result);
 })();
--- src/5/b.ts
+++ src/5/b.ts
@@ -1,8 +1,53 @@
-import { flow, splitByLine } from "../utils";
+import { splitByLine } from "../utils";
 import input from "./input";
 
 (function () {
-  const result = flow(input).pipe(splitByLine);
+  const lines = splitByLine(input);
+  const stackNumbersLineIndex = lines.findIndex((line) => line === "") - 1;
+  const lineOfStackNumber = lines[stackNumbersLineIndex];
+  const stackLines = lines.slice(0, stackNumbersLineIndex);
+  const instructionLines = lines.slice(stackNumbersLineIndex + 2);
+  const charPositions: number[] = [];
+
+  lineOfStackNumber.split("").forEach((char, i) => {
+    if (char !== " ") {
+      charPositions.push(i);
+    }
+  });
+
+  const stacks = new Map<number, string[]>();
+
+  stackLines.forEach((line) => {
+    charPositions.forEach((charPos, stackIdx) => {
+      const stackKey = stackIdx + 1;
+      const stack = stacks.get(stackKey) || [];
+      const val = line[charPos];
+
+      if (val !== " ") {
+        stack.push(val);
+        stacks.set(stackKey, stack);
+      }
+    });
+  });
+
+  instructionLines.forEach((line) => {
+    const [move, from, to] = line.split(" ").filter(Number).map(Number);
+
+    const fromStack = stacks.get(from);
+    const toStack = stacks.get(to);
+
+    if (fromStack && toStack) {
+      const moved = fromStack.splice(0, move);
+      toStack.unshift(...moved);
+    }
+  });
+
+  const result = Array.from(stacks.entries())
+    .sort((a, b) => a[0] - b[0])
+    .reduce((str, [_, stack]) => {
+      str += stack[0];
+      return str;
+    }, "");
 
   console.log(result);
 })();
