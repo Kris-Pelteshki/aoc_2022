3190a45c2b3ff13e71a5e0c734d89be8593a7e2a
Kris <kpelt@softserveinc.com>
2022-12-06 10:07:53 +0200

    day 6

--- dist/6/a.js
+++ dist/6/a.js
@@ -7,10 +7,10 @@ const utils_1 = require("../utils");
 const input_1 = __importDefault(require("./input"));
 (function () {
     const processData = (data, sequenceLength = 4) => {
-        const sequenceStartIndex = data
+        const startIndex = data
             .split("")
             .findIndex((_, i) => (0, utils_1.isUniqueSequenceOfElements)(data.slice(i, i + sequenceLength)));
-        return sequenceStartIndex + sequenceLength;
+        return startIndex !== -1 ? startIndex + sequenceLength : -1;
     };
     const result = (0, utils_1.flow)(input_1.default).pipe(processData);
     console.log(result);
--- src/6/b.ts
+++ src/6/b.ts
@@ -3,13 +3,13 @@ import input from "./input";
 
 (function () {
   const processData = (data: string, sequenceLength = 14) => {
-    const sequenceStartIndex = data
+    const startIndex = data
       .split("")
       .findIndex((_, i) =>
         isUniqueSequenceOfElements(data.slice(i, i + sequenceLength))
       );
 
-    return sequenceStartIndex + sequenceLength;
+    return startIndex !== -1 ? startIndex + sequenceLength : -1;
   };
 
   const result = flow(input).pipe(processData);
